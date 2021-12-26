15f357b62836c5907ecca0cfd08fff82553cb49c
Marten <derapfelesser@aol.com>
2021-12-26 22:46:43 +0100

    Initial commit

---
+++ src/cards.js
@@ -0,0 +1,44 @@
+const cards = [
+    {
+        name: 'Elliot Alderson',
+        imagePath: 'https://a-static.besthdwallpaper.com/elliot-alderson-tapet-2048x1536-9584_26.jpg', 
+    },
+    {
+        name: 'Tyrell Wellick',
+        imagePath: 'https://static.wikia.nocookie.net/villains/images/9/9d/Tyrell_Wellick_S4.jpg/revision/latest?cb=20191029131501',
+    },
+    {
+        name: 'Angela Moss',
+        imagePath: 'https://static.wikia.nocookie.net/mrrobot/images/4/4a/Tumblr_8a25712b9121eb8069ff575b842b9fc9_990d4100_1280.jpg/revision/latest?cb=20190928214750', 
+    },
+    {
+        name: 'Philip Price',
+        imagePath: 'https://static.wikia.nocookie.net/mrrobot/images/d/dd/Tumblr_e4e0d09f125afbdd165cd11b97acba0c_3553aa05_1280.jpg/revision/latest?cb=20190928214408',
+    },
+    {
+        name: 'Joanna Wellick',
+        imagePath: 'https://i.pinimg.com/originals/86/08/67/86086760c81683c98ff47c5db99f68de.jpg', 
+    },
+    {
+        name: 'Krista Gordon',
+        imagePath: 'https://static.wikia.nocookie.net/mrrobot/images/b/b6/Krista.png/revision/latest/top-crop/width/360/height/450?cb=20150603024920',
+    },        
+    {
+        name: 'Scott Knowles',
+        imagePath: 'https://static.wikia.nocookie.net/mrrobot/images/d/d2/Scott.png/revision/latest?cb=20150829181743', 
+    },
+    {
+        name: 'Gideon Goddard',
+        imagePath: 'https://static.wikia.nocookie.net/mrrobot/images/8/87/Gideon_001.jpg/revision/latest/top-crop/width/360/height/450?cb=20150607154334',
+    },
+    {
+        name: 'Fernando Vera',
+        imagePath: 'https://static.wikia.nocookie.net/mrrobot/images/d/d8/FVera.jpg/revision/latest?cb=20150809220022',
+    },
+    {
+        name: 'Darlene Alderson',
+        imagePath: 'https://static.wikia.nocookie.net/mrrobot/images/5/5c/Tumblr_96060a66d3af926ec3f638882b6ebb79_b2da2bc6_640.jpg/revision/latest?cb=20190928213622',
+    },
+]
+
+export default cards;
