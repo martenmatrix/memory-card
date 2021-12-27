1a42c38ae792c447e323b756cc5bd181d41798df
Marten <derapfelesser@aol.com>
2021-12-27 17:53:08 +0100

    Basic structure

--- src/App.js
+++ src/App.js
@@ -1,62 +1,112 @@
 import './App.css';
 import { useState, useEffect } from 'react';
+import cards from './cards';
 
 function Card(props) {
+    const name = props.name;
+    const imagePath = props.imagePath;
+    const onClick = props.onClick;
 
+    return (
+        <div className="card" data-name={name} onClick={onClick}>
+            <img src={imagePath} alt={name} />
+            <h2>{name}</h2>
+        </div>
+    )
 }
 
 function CardSection(props) {
-    const cards = [
-        {
-            name: 'Elliot Alderson',
-            imagePath: 'https://a-static.besthdwallpaper.com/elliot-alderson-tapet-2048x1536-9584_26.jpg', 
-        },
-        {
-            name: 'Tyrell Wellick',
-            imagePath: 'https://static.wikia.nocookie.net/villains/images/9/9d/Tyrell_Wellick_S4.jpg/revision/latest?cb=20191029131501',
-        },
-        {
-            name: 'Angela Moss',
-            imagePath: 'https://static.wikia.nocookie.net/mrrobot/images/4/4a/Tumblr_8a25712b9121eb8069ff575b842b9fc9_990d4100_1280.jpg/revision/latest?cb=20190928214750', 
-        },
-        {
-            name: 'Philip Price',
-            imagePath: 'https://static.wikia.nocookie.net/mrrobot/images/d/dd/Tumblr_e4e0d09f125afbdd165cd11b97acba0c_3553aa05_1280.jpg/revision/latest?cb=20190928214408',
-        },
-        {
-            name: 'Joanna Wellick',
-            imagePath: 'https://i.pinimg.com/originals/86/08/67/86086760c81683c98ff47c5db99f68de.jpg', 
-        },
-        {
-            name: 'Krista Gordon',
-            imagePath: 'https://static.wikia.nocookie.net/mrrobot/images/b/b6/Krista.png/revision/latest/top-crop/width/360/height/450?cb=20150603024920',
-        },        
-        {
-            name: 'Scott Knowles',
-            imagePath: 'https://static.wikia.nocookie.net/mrrobot/images/d/d2/Scott.png/revision/latest?cb=20150829181743', 
-        },
-        {
-            name: 'Gideon Goddard',
-            imagePath: 'https://static.wikia.nocookie.net/mrrobot/images/8/87/Gideon_001.jpg/revision/latest/top-crop/width/360/height/450?cb=20150607154334',
-        },
-        {
-            name: 'Fernando Vera',
-            imagePath: 'https://static.wikia.nocookie.net/mrrobot/images/d/d8/FVera.jpg/revision/latest?cb=20150809220022',
-        },
-        {
-            name: 'Darlene Alderson',
-            imagePath: 'https://static.wikia.nocookie.net/mrrobot/images/5/5c/Tumblr_96060a66d3af926ec3f638882b6ebb79_b2da2bc6_640.jpg/revision/latest?cb=20190928213622',
-        },
-    ]
+    const [shuffledCards, setShuffledCards] = useState([]);
+    const [usedCards, setUsedCards] = useState([]);
+    const [score, setScore] = props.scoreState;
+
+    function shuffleCards() {
+        const shuffledCards = cards.sort(() => Math.random() - 0.5);
+        setShuffledCards(shuffledCards);
+    }
+
+    function updateCurrentScore(hasLost) {
+        setScore(prevScore => ({
+            ...prevScore,
+            current: prevScore.current + 1,
+        }));
+
+        if (hasLost) {
+            setScore(prevScore => ({
+                    ...prevScore,
+                    current: 0,
+            }));
+        }
+    }
+
+    function updateBestScore() {
+        setScore(prevScore => {
+            const currentScore = prevScore.current;
+            if (prevScore.best < currentScore) {
+                return {
+                    ...prevScore,
+                    best: currentScore,
+                }
+            }
+            return prevScore;
+        });
+    }
+
+    function handleSelection(event) {
+        shuffleCards();
+        const cardName = event.currentTarget.dataset.name;
+        const cardIndex = usedCards.indexOf(cardName);
+        const hasLost = cardIndex !== -1;
+        updateBestScore();
+        updateCurrentScore(hasLost);
+        if (hasLost) {
+            setUsedCards([]);
+        } else {
+            setUsedCards(prevState => prevState.concat(cardName));
+        }
+    }
+
+    useEffect(() => {
+        shuffleCards();
+    }, [])
+
+    return (
+        <div className="card-section">
+            {shuffledCards.map((card, index) => {
+                return <Card key={index} name={card.name} imagePath={card.imagePath} onClick={handleSelection}/>
+            })}
+        </div>
+    )
 }
 
 function Scoreboard(props) {
+    const score = props.score.current;
+    const best = props.score.best;
 
+    return (
+        <div className="scoreboard">
+            <h1>Scoreboard</h1>
+            <h2>Highest Score: {best}</h2>
+            <h2>Current Score: {score}</h2>
+        </div>
+    )
 }
 
 function App() {
+    const [score, setScore] = useState(
+        {
+            current: 0,
+            best: 0,
+        }
+    )
+
     return (
       <div className="App">
+          <header>
+            <h1>Memory Game</h1>
+            <Scoreboard score={score}/>
+          </header>
+          <CardSection scoreState={[score, setScore]}/>
       </div>
     );
 }
--- src/cards.js
+++ src/cards.js
@@ -5,15 +5,15 @@ const cards = [
     },
     {
         name: 'Tyrell Wellick',
-        imagePath: 'https://static.wikia.nocookie.net/villains/images/9/9d/Tyrell_Wellick_S4.jpg/revision/latest?cb=20191029131501',
+        imagePath: 'https://static.wikia.nocookie.net/villains/images/9/9d/Tyrell_Wellick_S4.jpg',
     },
     {
         name: 'Angela Moss',
-        imagePath: 'https://static.wikia.nocookie.net/mrrobot/images/4/4a/Tumblr_8a25712b9121eb8069ff575b842b9fc9_990d4100_1280.jpg/revision/latest?cb=20190928214750', 
+        imagePath: 'https://static.wikia.nocookie.net/mrrobot/images/4/4a/Tumblr_8a25712b9121eb8069ff575b842b9fc9_990d4100_1280.jpg', 
     },
     {
         name: 'Philip Price',
-        imagePath: 'https://static.wikia.nocookie.net/mrrobot/images/d/dd/Tumblr_e4e0d09f125afbdd165cd11b97acba0c_3553aa05_1280.jpg/revision/latest?cb=20190928214408',
+        imagePath: 'https://static.wikia.nocookie.net/mrrobot/images/d/dd/Tumblr_e4e0d09f125afbdd165cd11b97acba0c_3553aa05_1280.jpg',
     },
     {
         name: 'Joanna Wellick',
@@ -21,23 +21,23 @@ const cards = [
     },
     {
         name: 'Krista Gordon',
-        imagePath: 'https://static.wikia.nocookie.net/mrrobot/images/b/b6/Krista.png/revision/latest/top-crop/width/360/height/450?cb=20150603024920',
+        imagePath: 'https://static.wikia.nocookie.net/mrrobot/images/b/b6/Krista.png',
     },        
     {
         name: 'Scott Knowles',
-        imagePath: 'https://static.wikia.nocookie.net/mrrobot/images/d/d2/Scott.png/revision/latest?cb=20150829181743', 
+        imagePath: 'https://static.wikia.nocookie.net/mrrobot/images/d/d2/Scott.png', 
     },
     {
         name: 'Gideon Goddard',
-        imagePath: 'https://static.wikia.nocookie.net/mrrobot/images/8/87/Gideon_001.jpg/revision/latest/top-crop/width/360/height/450?cb=20150607154334',
+        imagePath: 'https://static.wikia.nocookie.net/mrrobot/images/8/87/Gideon_001.jpg',
     },
     {
         name: 'Fernando Vera',
-        imagePath: 'https://static.wikia.nocookie.net/mrrobot/images/d/d8/FVera.jpg/revision/latest?cb=20150809220022',
+        imagePath: 'https://static.wikia.nocookie.net/mrrobot/images/d/d8/FVera.jpg',
     },
     {
         name: 'Darlene Alderson',
-        imagePath: 'https://static.wikia.nocookie.net/mrrobot/images/5/5c/Tumblr_96060a66d3af926ec3f638882b6ebb79_b2da2bc6_640.jpg/revision/latest?cb=20190928213622',
+        imagePath: 'https://static.wikia.nocookie.net/mrrobot/images/5/5c/Tumblr_96060a66d3af926ec3f638882b6ebb79_b2da2bc6_640.jpg',
     },
 ]
 
